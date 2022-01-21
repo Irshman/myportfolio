66b37dd5cb77a740df3c1c11f421e1781a18182c
Irshman <76690755+Irshman@users.noreply.github.com>
2022-01-21 20:57:45 +0200

    add about me block

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,46 @@
 const links = document.querySelectorAll(".sidebar__link");
+const about = document.querySelector(".about");
+const work = document.querySelector(".works");
+const skills = document.querySelector(".skills");
 
 links.forEach((item) => {
     item.addEventListener('click', (e) => {
-        item.classList.add('sidebar__link--active');
+
+        if(e.target.id === "work") {
+          about.style.display = "none";
+          skills.style.display = "none";
+          work.style.display = "flex";
+
+          links.forEach(item => {
+            item.classList.remove('sidebar__link--active');
+          });
+
+          e.target.classList.add('sidebar__link--active');
+        }
+
+        if(e.target.id === "about") {
+          about.style.display = "flex";
+          work.style.display = "none";
+          skills.style.display = "none";
+
+          links.forEach(item => {
+            item.classList.remove('sidebar__link--active');
+          });
+
+          e.target.classList.add('sidebar__link--active');
+        }
+
+        if(e.target.id === "skills") {
+          about.style.display = "none";
+          work.style.display = "none";
+          skills.style.display = "flex";
+
+          links.forEach(item => {
+            item.classList.remove('sidebar__link--active');
+          });
+
+          e.target.classList.add('sidebar__link--active');
+        }
     })
 })
+
